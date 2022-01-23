f5f5815d91edde96b84cb3286d3af271971a7360
= <=>
2022-01-23 11:47:31 -0700

    Transition from start to step 2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,60 @@
-const rulesButton = document.querySelector("button.rules");
-const modal = document.querySelector(".modal");
+/* elements */
+
+const rulesButton = document.getElementById("rules");
+const modalContainer = document.querySelector(".modal-container");
 const modalCloseButton = document.querySelector(".modal-close");
+const tokenContainers = document.querySelectorAll(".token-container");
+const gameStartSection = document.querySelector("section.game-start");
+const playAgainButton = document.getElementById("playAgain");
+
+const gameActive = {
+  section: document.querySelector("section.game-active"),
+  tokenContainers: document.querySelectorAll(".game-active-token-container"),
+};
+
+/* Functions */
+
+const goToStep = (step, playerSelection) => {
+  switch (step) {
+    case 1:
+      gameStartSection.classList.remove("hidden");
+      gameActive.section.classList.add("hidden");
+      break;
+    case 2:
+      displayPlayerSelection(playerSelection);
+      gameStartSection.classList.add("hidden");
+      gameActive.section.classList.remove("hidden");
+
+      determineComputerSelection();
+      break;
+  }
+};
+
+const displayPlayerSelection = (playerSelection) => {
+  document
+    .querySelector(`.game-active-token-container.token-container-${playerSelection}`)
+    .classList.remove("hidden");
+};
+
+const determineComputerSelection = () => {};
 
 /* Event Handlers */
 
 rulesButton.addEventListener("click", (e) => {
-  modal.classList.toggle("hidden");
+  modalContainer.classList.toggle("hidden");
 });
 
 modalCloseButton.addEventListener("click", (e) => {
-  modal.classList.add("hidden");
+  modalContainer.classList.add("hidden");
+});
+
+tokenContainers.forEach((token) => {
+  token.addEventListener("click", function (e) {
+    const playerSelection = this.dataset.token;
+    goToStep(2, playerSelection);
+  });
+});
+
+playAgainButton.addEventListener("click", (e) => {
+  goToStep(1);
 });
