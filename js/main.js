b8fbc104b21dd118f87e564bdcfefe2c2133f288
Travis Peters <travis@tkopeters.com>
2022-01-25 17:45:55 -0700

    finished desktop implementation

--- js/main.js
+++ js/main.js
@@ -6,17 +6,70 @@ const modalCloseButton = document.querySelector(".modal-close");
 const tokenContainers = document.querySelectorAll(".token-container");
 const gameStartSection = document.querySelector("section.game-start");
 const playAgainButton = document.getElementById("playAgain");
-
+const playerToken = document.querySelector(".player-token");
+const computerToken = document.querySelector(".computer-token");
+const gameCompleted = document.querySelector(".game-completed");
 const gameActive = {
   section: document.querySelector("section.game-active"),
   tokenContainers: document.querySelectorAll(".game-active-token-container"),
 };
+let score = 0;
+
+const options = ["rock", "paper", "scissors"];
+
+const optionElements = {
+  paper: `
+    <div class="token-outer-container">
+      <div class="game-active-token-inner-container token-container-paper">
+        <div class="game-active-token">
+          <img src="../images/icon-paper.svg" alt="Paper" />
+        </div>
+      </div>
+    </div>
+  `,
+  rock: `    
+    <div class="token-outer-container">
+      <div class="game-active-token-inner-container token-container-rock">
+        <div class="game-active-token">
+          <img src="../images/icon-rock.svg" alt="Rock" />
+        </div>
+      </div>
+    </div>
+  `,
+  scissors: `    
+  <div class="token-outer-container">
+    <div class="game-active-token-inner-container token-container-scissors">
+      <div class="game-active-token">
+        <img src="../images/icon-scissors.svg" alt="Scissors" />
+      </div>
+    </div>
+  </div>
+`,
+};
 
 /* Functions */
 
-const goToStep = (step, playerSelection) => {
+const init = () => {
+  //check local storage
+  const tkoRPS = localStorage.getItem("tko-rps");
+
+  if (tkoRPS === null) {
+    localStorage.setItem("tko-rps", "0");
+    score = 0;
+  } else {
+    score = parseInt(tkoRPS);
+  }
+
+  document.querySelector(".score").innerHTML = score;
+};
+
+const goToStep = async (step, playerSelection) => {
   switch (step) {
     case 1:
+      computerToken.innerHTML = `<div class="computer-token-placeholder"></div>`;
+      computerToken.classList.remove("winner");
+      playerToken.classList.remove("winner");
+      gameCompleted.classList.add("hidden");
       gameStartSection.classList.remove("hidden");
       gameActive.section.classList.add("hidden");
       break;
@@ -25,19 +78,67 @@ const goToStep = (step, playerSelection) => {
       gameStartSection.classList.add("hidden");
       gameActive.section.classList.remove("hidden");
 
-      determineComputerSelection();
+      const computerSelectionIndex = await determineComputerSelection();
+
+      displayComputerSelection(options[computerSelectionIndex]);
+
+      determineWinner(playerSelection, options[computerSelectionIndex]);
+
+      gameCompleted.classList.remove("hidden");
       break;
   }
 };
 
 const displayPlayerSelection = (playerSelection) => {
-  document
-    .querySelector(`.game-active-token-container.token-container-${playerSelection}`)
-    .classList.remove("hidden");
+  const playerToken = document.querySelector(".player-token");
+  playerToken.innerHTML = optionElements[playerSelection];
 };
 
-const determineComputerSelection = () => {};
+const determineComputerSelection = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const computerSelection = Math.floor(Math.random() * options.length);
+      resolve(computerSelection);
+    }, 500);
+  });
+};
 
+const displayComputerSelection = (computerSelection) => {
+  computerToken.innerHTML = optionElements[computerSelection];
+};
+
+const determineWinner = (playerSelection, computerSelection) => {
+  let resultText = "";
+  //Tie
+  if (playerSelection == computerSelection) {
+    resultText = "It's a tie!";
+    document.querySelector(".result").innerHTML = resultText;
+    return;
+  }
+
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    resultText = "You win!";
+    playerToken.classList.add("winner");
+    updateScore(1);
+  } else {
+    resultText = "You lose!";
+    computerToken.classList.add("winner");
+    updateScore(-1);
+  }
+
+  document.querySelector(".result").innerHTML = resultText;
+  return;
+};
+
+const updateScore = (amount) => {
+  score = score + amount;
+  document.querySelector(".score").innerHTML = score;
+  localStorage.setItem("tko-rps", score);
+};
 /* Event Handlers */
 
 rulesButton.addEventListener("click", (e) => {
@@ -58,3 +159,5 @@ tokenContainers.forEach((token) => {
 playAgainButton.addEventListener("click", (e) => {
   goToStep(1);
 });
+
+init();
